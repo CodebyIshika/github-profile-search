9a8a9972ef22c4b2e7afc43b69dcae23ab806740
CodeByIshika <ishika11518@gmail.com>
2024-06-14 09:44:38 -0500

    Start github-finder project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Search from "./components/Search";
+import User from "./components/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+      <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="/user/:username" element={<User />} />
+        </Routes>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Search.js
@@ -0,0 +1,63 @@
+// Example in Search.js
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const Search = () => {
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  // Accessing the environment variable
+  const token = process.env.REACT_APP_GITHUB_TOKEN;
+
+  const handleInputChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+
+    if (username.trim() === '') {
+      setError('Username is required');
+      return;
+    }
+
+    try {
+      const options = { headers: { Authorization: `Bearer ${token}` } };
+      const response = await axios.get(`https://api.github.com/users/${username}`, options);
+
+      if (response.status === 200) {
+        navigate(`/user/${username}`);
+      }
+    } catch (err) {
+      setError('No user found');
+    }
+  };
+
+  return (
+    <div className="search-container">
+      <img 
+          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" 
+          alt="GitHub Icon" 
+          className="github-icon" 
+      />
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Enter GitHub username"
+            value={username}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+      {error && <p>{error}</p>}
+      {!error && <p>Welcome to GitHub Finder</p>}
+    </div>
+  );
+};
+
+export default Search;
